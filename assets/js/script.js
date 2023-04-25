df63271ff4e6cf623449dadd5d5341f6049b8550
Yasmeen <127054048+Yasmeeeenn@users.noreply.github.com>
2023-04-25 14:34:14 -0500

    added search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,25 @@
-var input = document.getElementById("input");
-var search = document.getElementById("searchButton");
-var requestURL = "https://api.lyrics.ovh/v1/artist/title";
+var searchInput = document.getElementById("search");
 
-
-function searchClick() {
+document.querySelector("form.search-bar").addEventListener("submit", function(e){
+   e.preventDefault();
+    console.log(searchInput.value);
+});
    
-    // calls the YT data API
-    // GET https://www.googleapis.com/youtube/v3/search
+    
 
-    console.log(input.value);
 
-    /* 1) triggers YT data API
-    2) returns list of links
-    3) triggers lyrics API
-    4) stores lyrics API
-    5) triggers dynamic creation of JS search results box */
-}
 
-search.addEventListener('click', searchClick);
+
+// calls the YT data API
+// GET https://www.googleapis.com/youtube/v3/search
+
+    
+
+/* 1) triggers YT data API
+2) returns list of links
+3) triggers lyrics API
+4) stores lyrics API
+5) triggers dynamic creation of JS search results box */
+
+
+
